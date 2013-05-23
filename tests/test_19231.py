b3b9f8a51be1771a52537fe71fc1df39825b06fe
roy <roy.collings@sogeti.com>
2013-05-23 12:00:53 +0200

    Fixed 19231

--- tests/test_19231.py
+++ tests/test_19231.py
@@ -5,10 +5,13 @@ import sys
 sys.path.insert(1, "./")
 from gaiatest   import GaiaTestCase
 from OWDTestToolkit import *
+from marionette import Actions
 
 
 class test_19231(GaiaTestCase):
     _Description = "[HOME SCREEN] Verify that the user can uninstall a everything.me app through the grid edit mode."
+    
+    _appName = "Wikipedia"
 
     def setUp(self):
         #
@@ -16,8 +19,10 @@ class test_19231(GaiaTestCase):
         #
         GaiaTestCase.setUp(self)
         self.UTILS      = UTILS(self)
+        self.actions    = Actions(self.marionette)
         self.settings   = AppSettings(self)
-        self.market     = AppMarket(self)
+#         self.market     = AppMarket(self)
+        self.eme        = AppEverythingMe(self)
                 
         #
         # Set timeout for element searches.
@@ -36,15 +41,29 @@ class test_19231(GaiaTestCase):
         # Get a conection.
         #
         self.UTILS.getNetworkConnection()
-        self.UTILS.uninstallApp("Wikipedia")
+        self.UTILS.uninstallApp(self._appName)
                 
         #
         # Get the app.
         #
-        self.market.launch()
-        self.UTILS.TEST(self.market.installApp("Wikipedia", "tfinc"),
-                        "App is installed.")
+        self.eme.launch()
+        x = self.eme.searchForApp(self._appName)
         
-        self.UTILS.uninstallApp("Wikipedia")
+        self.UTILS.TEST(x, "Icon for " + self._appName + " is found.", True)
+        
+        x = self.UTILS.getElement( ("xpath", DOM.EME.search_result_icon_xpath % self._appName),
+                                   self._appName + " icon")
+        
+        self.actions.press(x).wait(2).release()
+        self.actions.perform()
+        
+        self.marionette.switch_to_frame()
+        x = self.UTILS.getElement(DOM.GLOBAL.modal_ok_button, "OK button")
+        x.tap()
+        
+        time.sleep(2)
+        self.UTILS.goHome()
+        
+        self.UTILS.uninstallApp(self._appName)
         
         
